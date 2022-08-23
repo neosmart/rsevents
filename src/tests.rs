a217ff7aa63630578fa3f7cb61e2f1c4e667206a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-08-23 13:46:50 -0500

    Expand tests and add miri driver to Makefile

--- src/tests.rs
+++ src/tests.rs
@@ -1,37 +1,57 @@
 use crate::*;
-use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
 use std::thread;
 
 #[test]
-fn sanity_check() {
+fn sanity_check_set() {
     let event = RawEvent::new(AVAILABLE_BIT);
-    assert_eq!(true, event.try_unlock_one());
-
-    let event = RawEvent::new(0);
-    assert_eq!(false, event.try_unlock_one());
+    assert_eq!(true, event.test_try_unlock_one());
 }
 
 #[test]
-fn basic_locking() {
+fn sanity_check_unset() {
     let event = RawEvent::new(0);
-    event.set_all();
-    assert_eq!(true, event.try_unlock_one());
+    assert_eq!(false, event.test_try_unlock_one());
 }
 
 #[test]
-fn basic_unlocking() {
+fn basic_set_unlock_one() {
+    let event = RawEvent::new(0);
+    event.set_one();
+    assert_eq!(true, event.test_try_unlock_one());
+}
+
+#[test]
+fn basic_set_unlock_all() {
+    let event = RawEvent::new(0);
+    event.set_one();
+    assert_eq!(true, event.try_unlock_all());
+    assert_eq!(true, event.try_unlock_all());
+}
+
+#[test]
+fn basic_reset_one() {
     let event = RawEvent::new(AVAILABLE_BIT);
     event.reset();
-    assert_eq!(false, event.try_unlock_one());
+    assert_eq!(false, event.test_try_unlock_one());
 }
 
 #[test]
-fn basic_double_unlock() {
+fn basic_reset_all() {
+    let event = RawEvent::new(AVAILABLE_BIT);
+    event.reset();
+    assert_eq!(false, event.try_unlock_all());
+}
+
+#[test]
+fn basic_double_unlock_auto() {
     let event = AutoResetEvent::new(State::Set);
     assert_eq!(true, event.wait0());
     assert_eq!(false, event.wait0());
+}
 
+#[test]
+fn basic_double_unlock_manual() {
     let event = ManualResetEvent::new(State::Set);
     assert_eq!(true, event.wait0());
     assert_eq!(true, event.wait0());
@@ -55,35 +75,42 @@ fn suspend_and_resume() {
     };
     event2.wait();
     event1.set();
-    thread.join().ok();
+    thread.join().unwrap();
 }
 
 #[test]
-/// Verify that when a thread is unlocked only one waiting thread gets through.
-fn single_thread_release() {
+/// Verify that when an [`AutoResetEvent`] is made available only one waiting thread gets through at
+/// a time, and verify cache coherence of unsynchronized access to a non-atomic variable is
+/// maintained by the calls to set() and wait().
+fn auto_reset_cache_coherence() {
+    const THREAD_COUNT: usize = 20;
     let event = Arc::new(AutoResetEvent::new(State::Unset));
     // event2 is used to signal that a waiter has finished
     let event2 = Arc::new(AutoResetEvent::new(State::Unset));
-    let succeed_count = Arc::new(AtomicUsize::new(0));
-    succeed_count.store(0, Ordering::Relaxed);
+
+    // We are not using an atomic here to ensure that AutoResetEvent set/wait()
+    // pairs guarantee memory order.
+    static mut SUCCEED_COUNT: usize = 0_usize;
 
     let create_waiter = || {
         let event = event.clone();
         let event2 = event2.clone();
-        let succeed_count = succeed_count.clone();
         thread::spawn(move || {
+            // Wait for exclusive access
             event.wait();
-            succeed_count.fetch_add(1, Ordering::AcqRel);
+            unsafe { SUCCEED_COUNT += 1; }
+            // Allow another thread in
             event2.set();
         })
     };
 
     // Create 50 threads that will contend for the event
-    for _ in 0..50 {
-        create_waiter();
+    let mut join_handles = Vec::new();
+    for _ in 0..THREAD_COUNT {
+        join_handles.push(create_waiter());
     }
 
-    // Hopefully let just one event through
+    // Hopefully let just one event through, observing the initial value
     event.set();
 
     // Yield for 100 time slices
@@ -91,9 +118,13 @@ fn single_thread_release() {
         std::thread::yield_now();
     }
 
-    event2.wait();
-    assert_eq!(succeed_count.load(Ordering::Acquire), 1);
-    event.set();
-    event2.wait();
-    assert_eq!(succeed_count.load(Ordering::Acquire), 2);
+    for i in 0..THREAD_COUNT {
+        event2.wait();
+        assert_eq!(unsafe { SUCCEED_COUNT }, i + 1);
+        event.set();
+    }
+
+    for jh in join_handles {
+        jh.join().unwrap();
+    }
 }
