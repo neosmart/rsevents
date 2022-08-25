0d2da84abad7cdf3b3500b5b43bd92a81e5d5ceb
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-08-25 14:13:41 -0500

    Update examples/readme.rs to feel more complete
    This revised approach demonstrates the usage of `Awaitable::wait_for()` with
    timeouts, and demonstrates a reasonable usage that isn't wanting for
    `WaitForMultipleObject` support with worker thread aborts handled via a timeout.
    
    The consistency and atomicity guarantees of `AutoResetEvent` are allowed to
    shine by using raw memory access rather than wrapping `ThreadMessage` in an
    `RwLock`.

--- examples/readme.rs
+++ examples/readme.rs
@@ -1,85 +1,89 @@
-use std::sync::RwLock;
+/// This example illustrates the use of [`AutoResetEvent`] to dispatch work to
+/// one worker at a time from a thread pool, with the events guaranteeing memory
+/// consistency, efficient wait, wait-with-timeout, and exclusive dispatch.
+///
+/// [`AutoResetEvent`]: rsevents::AutoResetEvent
+
+use std::time::Duration;
 use rsevents::{Awaitable, AutoResetEvent, EventState};
 
-#[derive(Default, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 enum ThreadMessage {
-    // Used in lieu of wrapping `ThreadMessage` in an `Option`
-    #[default]
+    /// Used in lieu of wrapping `ThreadMessage` in an `Option`
     None,
-    // Indicates that all worker threads should abort
-    Abort,
-    // Hands off a value to a worker thread for processing
+    /// Hands off a value to a worker thread for processing
     Input(u32),
 }
 
+// Events are cheap: each one is only a single byte!
 static TASK_READY: AutoResetEvent = AutoResetEvent::new(EventState::Unset);
 static DISPATCHED: AutoResetEvent = AutoResetEvent::new(EventState::Unset);
 
 pub fn main() {
-    let shared: RwLock<ThreadMessage> = RwLock::new(ThreadMessage::None);
-    std::thread::scope(|scope| {
-        const THREAD_COUNT: usize = 3;
-        for thread_idx in 0..THREAD_COUNT {
-            let thread_idx = thread_idx;
-            let shared = &shared;
-            scope.spawn(move || {
-                loop {
-                    // Wait efficiently for the main thread to signal
-                    // _one_ (and only one) worker thread to do something
-                    // with the shared state.
-                    TASK_READY.wait();
+    // The events above synchronize access to this !Sync, !Send shared state
+    static mut SHARED: ThreadMessage = ThreadMessage::None;
 
-                    let shared = {
-                        *shared.try_read().expect("AutoResetEvent provides synchronization")
-                    };
-
-                    // Signal to the main thread that we've taken the value
-                    // and that it can overwrite `shared` at its leisure. Processing
-                    // afterwards can take as long as it needs.
-                    DISPATCHED.set();
-
-                    match shared {
-                        ThreadMessage::None => unreachable!(),
-                        ThreadMessage::Abort => break,
-                        ThreadMessage::Input(value) =>
-                            eprintln!("Thread {thread_idx} handling value {value}"),
-                    }
+    const THREAD_COUNT: usize = 3;
+    let mut threads = Vec::with_capacity(THREAD_COUNT);
+    for thread_idx in 0..THREAD_COUNT {
+        let join_handle = std::thread::spawn(move || {
+            loop {
+                // Wait efficiently for the main thread to signal _one_ (and
+                // only one) worker thread at a time.
+                if !TASK_READY.wait_for(Duration::from_millis(500)) {
+                    // When there's not enough work, let the thread pool drain
+                    break;
                 }
 
-                eprintln!("Thread {thread_idx} exiting");
-            });
-        }
+                // This is safe because our events guarantee that
+                // * one thread will be accessing this variable at a time
+                // * shared memory will be consistent betwixt a call to
+                //   event.set() from one thread and a call to event.wait()
+                //   from another.
+                let work_msg = unsafe { *(&SHARED as *const ThreadMessage) };
 
-        // Generate some "random" values at an interval, dispatching each
-        // exactly once to exactly one worker thread.
-        for value in [4, 8, 15, 16, 23, 42] {
-            {
-                let mut shared = shared.try_write()
-                    .expect("AutoResetEvent provides synchronization");
-                *shared = ThreadMessage::Input(value);
+                // Signal to the main thread that we've taken the value and that
+                // it can overwrite `shared` at its leisure. Afterwards,
+                // processing can take as long as it needs.
+                DISPATCHED.set();
+
+                match work_msg {
+                    ThreadMessage::None =>
+                        unreachable!("The AutoResetEvent guarantees against this"),
+                    ThreadMessage::Input(value) =>
+                        eprintln!("Thread {thread_idx} handling value {value}"),
+                }
             }
-            // Signal a currently idle or the next idle worker thread to
-            // handle this value.
-            TASK_READY.set();
+        });
+        threads.push(join_handle);
+    }
 
-            // Wait for a worker thread to signal it has received the
-            // payload and we can stomp the `shared` value.
-            DISPATCHED.wait();
+    // Generate some "random" values at an interval, dispatching each exactly
+    // once to exactly one worker thread.
+    for value in [4, 8, 15, 16, 23, 42] {
+        unsafe {
+            // It's perfectly safe to access - and even write - to SHARED here
+            // because our two events guarantee exclusive access (as
+            // AutoResetEvents wake one thread at a time) and take care of
+            // synchronizing the memory plus any cache coherence issues between
+            // the writing thread (this one) and the reading worker thread.
+            *(&mut SHARED as * mut _) = ThreadMessage::Input(value);
         }
 
-        // Signal all threads to exit
-        for _ in 0..THREAD_COUNT {
-            {
-                let mut shared = shared.try_write().expect("AutoResetEvent provides synchronization");
-                *shared = ThreadMessage::Abort;
-            }
-            TASK_READY.set();
-            DISPATCHED.wait();
-        }
+        // Signal a currently idle or the next idle worker thread to handle this
+        // value.
+        TASK_READY.set();
 
-        // The spawned threads are automatically joined at the end of the
-        // scope.
-    });
+        // Remember that work is usually dispatched faster than it can be
+        // handled!
+        // Wait for a worker thread to signal it has received the payload and we
+        // can stomp the `SHARED` value and dispatch work to the next worker.
+        DISPATCHED.wait();
+    }
 
+    // Wait for the thread pool to drain and exit
+    for jh in threads {
+        jh.join().expect("Worker thread panicked!");
+    }
     eprintln!("All work completed - exiting!")
 }
